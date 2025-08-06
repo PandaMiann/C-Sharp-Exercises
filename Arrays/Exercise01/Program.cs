780994e6655fc509012d04e79329e58ad4758629
PandaMian <PandaMian@seznam.cz>
2025-08-06 13:56:01 +0200

    Write code for Exercise 01

--- Arrays/Exercise01/Program.cs
+++ Arrays/Exercise01/Program.cs
@@ -2,8 +2,34 @@
 
 class Program
 {
+    public static void LoadNumbers(int[] array)
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            Console.Write($"Enter number {i}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    public static void FindMax(int[] array)
+    {
+        int maxValue = int.MinValue;
+        foreach (int number in array)
+        {
+            if (number > maxValue)
+            {
+                maxValue = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {maxValue}");
+    
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] array = new int[11];
+        LoadNumbers(array);
+        FindMax(array);
+
+
     }
 }
